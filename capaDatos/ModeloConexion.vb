394fa1a9ebfc65c9a954ee478c31f5fc7d9fa357
Ruben Gainza <ruben.gainza@gmail.com>
2020-06-06 20:02:59 -0300

    Se resolvio el problema de la dulpicacion de patologias en el diagnostico final. Probado OK

--- capaDatos/ModeloConexion.vb
+++ capaDatos/ModeloConexion.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.Odbc
 Public Class ModeloConexion
-
-    Dim conexion As New Odbc.OdbcConnection("DRIVER=MySQL ODBC 5.3 ANSI Driver;UID=pepe;PWD=Pepe1234*;PORT=3306;DATABASE=proyectocds;SERVER=192.168.1.15")
+    'Dim conexion As New MySqlConnection("server=192.168.1.15;database=proyectocds;user=pepe;password=Pepe1234*;port=3306")
+    Dim conexion As New Odbc.OdbcConnection("DRIVER=MySQL ODBC 5.3 ANSI Driver;UID=marcos;PWD=admin;PORT=3306;DATABASE=dbTriage;SERVER=localhost")
     Public Function Abrir() As Odbc.OdbcConnection
         Try
             conexion.Open()
