7d51ebfc2a98a1278e626749a551be515ccc7e0e
mathias.torterolo <mathias.torterolo@gmail.com>
2020-06-14 11:55:59 -0300

    Se cera AltaPatologia() en ControladorPatologia

--- capaLogica/ControladorPatologias.vb
+++ capaLogica/ControladorPatologias.vb
@@ -3,6 +3,16 @@ Imports capaDatos
 
 Public Module ControladorPatologias
 
+    Public Sub AltaPatologia(nombre As String, ponderacion As Integer, descripcion As String, activo As Integer)
+        Dim p As New ModeloPatologia
+        p.Nombre = nombre
+        p.Ponderacion = ponderacion
+        p.Descripcion = descripcion
+        p.activo = activo
+        p.GuaradrPatologia()
+
+    End Sub
+
     Public Function ListarPatologias() As DataTable
         Dim p As New ModeloPatologia
         Return p.TraeDatosPatologiasDeBD()
