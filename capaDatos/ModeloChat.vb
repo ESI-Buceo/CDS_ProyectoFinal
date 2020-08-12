e6ae572f2c7e0c0e8ee761e55ee81ba0814f4c51
Andrea Bazzini <andreabaz2012@gmail.com>
2020-08-12 14:50:41 -0300

    se creo controlador y modelo para recibir mensajes de Chat

--- capaDatos/ModeloChat.vb
+++ capaDatos/ModeloChat.vb
@@ -1,3 +1,46 @@
-﻿Public Class ModeloChat
+﻿Imports System.Data.Odbc
 
+Public Class ModeloChat
+    Inherits ModeloConexion
+
+    Public idSesion As String
+    Public docidentidadPaciente As String
+    Public docidentidadMedico As String
+    Public emisor As String
+    Public mensaje As String
+
+    Public Sub New(ByVal uid As String, pwd As String)
+        MyBase.New(uid, pwd)
+    End Sub
+
+
+    Public Function RecibirMensajes(ByVal emisor As String)
+        Dim tablaMensajes As New DataTable
+        Comando.CommandText = "SELECT * FROM chat WHERE idSesion = " & ModeloDiagnostico.CodigoDiagnostico & " AND leido = 0 AND emisor <> '" & emisor & "'"
+        Reader = Comando.ExecuteReader
+        tablaMensajes.Load(Reader)
+        CerrarConexion()
+        Return tablaMensajes
+    End Function
+
+
+    Public Function RecibirMensajes(ByVal idSesion As String, emisor As String)
+        'Lo utiliza la app del paciente
+        Dim tablaMensajes As New DataTable
+        Comando.CommandText = "SELECT * FROM chat WHERE idSesion = " & idSesion & " AND leido = 0 AND emisor <> '" & emisor & "'"
+        Reader = Comando.ExecuteReader
+        tablaMensajes.Load(Reader)
+        CerrarConexion()
+        Return tablaMensajes
+    End Function
+
+
+    Public Function RecibirTodosMensajes(ByVal idSesion As String)
+        Dim tablaMensajes As New DataTable
+        Comando.CommandText = "SELECT * FROM chat WHERE idSesion = " & idSesion
+        Reader = Comando.ExecuteReader
+        tablaMensajes.Load(Reader)
+        CerrarConexion()
+        Return tablaMensajes
+    End Function
 End Class
