e2f6a9648620947709aa6a1df9cbca44f140f29c
Ruben Gainza <ruben.gainza@gmail.com>
2020-08-12 13:13:13 -0300

    Se actualizaron los formularios de la aplicacion de medico.

--- appMedico/My Project/Resources.Designer.vb
+++ appMedico/My Project/Resources.Designer.vb
@@ -133,6 +133,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property logo_App() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("logo_App", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property logo_app_grises() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("logo-app-grises", resourceCulture)
