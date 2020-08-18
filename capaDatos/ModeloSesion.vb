b25ca4860d30f9d1746f0a725cff658a3ae50798
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-08-18 11:59:38 -0300

    "Se crea metodo guardar sesion, Persistentecia de datos tabla sesion"

--- capaDatos/ModeloSesion.vb
+++ capaDatos/ModeloSesion.vb
@@ -1,5 +1,19 @@
 ﻿Public Class ModeloSesion
     Inherits ModeloConexion
 
+    Public IdSesion As String = ModeloDiagnostico.CodigoDiagnostico
+    Public Prioridad As String = ModeloDiagnostico.Ponderacion
+    Public Estado As String
+
+    Public Sub New(ByVal uid As String, pwd As String)
+        MyBase.New(uid, pwd)
+    End Sub
+
+    Public Sub GuardarSesion()
+        'Guarda el inicio de una sesion con su prioridad
+        Comando.CommandText = "INSERT INTO sesion (idSesion, prioridad) VALUES(" & Me.IdSesion & ", " & Me.Prioridad & ")"
+        Comando.ExecuteNonQuery()
+        CerrarConexion()
+    End Sub
 
 End Class
