aa73810594376a3aa3f6a073170683a813b2ab4a
mathias.torterolo <mathias.torterolo@gmail.com>
2020-09-13 21:44:51 -0300

    Se crea testGuardarAdministrativo

--- UnitTest/TestModeloAdministrativo.vb
+++ UnitTest/TestModeloAdministrativo.vb
@@ -17,4 +17,48 @@ Imports capaDatos
 
     End Sub
 
+    <TestMethod()> Public Sub TestGuardarAdministrativo()
+        Dim Resultado As Boolean
+        Dim Telefono As New DataTable
+        Telefono.Columns.Add("Telefono")
+        Telefono.Rows.Add("097050602")
+        Telefono.Rows.Add("098451212")
+        Dim a As New ModeloAdministrativo("22222222", "Ge.22222222")
+        a.Documento = "40717849"
+        a.Email = "nuevo@admin.com"
+        a.Nombres = "maria"
+        a.Apellidos = "calambres"
+        a.Calle = "colombes"
+        a.Numero = "1245"
+        a.Barrio = "jacinto vera"
+        a.Esquina = "del infinito"
+        a.Apartamento = "3"
+        a.FechaNacimiento = "1976-12-20"
+        a.NumeroEmpleado = "7894"
+        a.Telefonos = Telefono
+
+        Try
+            a.GuardarAdministrativo()
+            Resultado = True
+        Catch ex As Odbc.OdbcException
+            Resultado = False
+        End Try
+
+        Assert.IsTrue(Resultado)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestCrearUsuarioBD()
+        Dim a As New ModeloAdministrativo("11111111", "Ge.11111111")
+        Dim Resultado As Boolean
+        Try
+            a.CrearUsuarioBD()
+            Resultado = True
+        Catch ex As Exception
+            Resultado = False
+        End Try
+        Assert.IsTrue(Resultado)
+
+    End Sub
+
 End Class
