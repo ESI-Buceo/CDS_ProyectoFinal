4b80f5d57d9cc30a8638ca1ae450fc24cc1600c0
Mathias Torterolo <mathias.torterolo@gmail.com>
2020-10-10 12:59:24 -0300

    TestModelochat quedo pronto

--- UnitTest/TestModeloChat.vb
+++ UnitTest/TestModeloChat.vb
@@ -23,7 +23,7 @@ Imports System.Threading
         Thread.Sleep(20)
     End Sub
 
-    <TestMethod()> Public Sub TestRecibirMensajes()
+    <TestMethod()> Public Sub TestRecibirMensajesPorDiagnostico()
         ModeloDiagnostico.CodigoDiagnostico = "161348021"
 
         Dim Resultado As Boolean
@@ -39,6 +39,7 @@ Imports System.Threading
     End Sub
 
     <TestMethod()> Public Sub TestMarcarMensajeLeido()
+        'se pasa por parametro el id del chat para marcarlo como leido 
         Dim Resultado As Boolean
         Try
             c.MarcarMensajeLeido("16")
@@ -51,12 +52,49 @@ Imports System.Threading
         Thread.Sleep(20)
     End Sub
 
-
-
     <TestMethod()> Public Sub TestCantidadDeChats()
         Dim Resultado As Boolean
         Try
             c.CantidadDeChats("19248371")
+            Resultado = True
+        Catch ex As Exception
+            Resultado = False
+        End Try
+
+        Assert.IsTrue(Resultado)
+        Thread.Sleep(20)
+    End Sub
+
+    <TestMethod()> Public Sub TestListaHistoricaChatPaciente()
+        Dim Resultado As Boolean
+        Try
+            c.ListaHistoricaChatPaciente("19248371")
+            Resultado = True
+        Catch ex As Exception
+            Resultado = False
+        End Try
+
+        Assert.IsTrue(Resultado)
+        Thread.Sleep(20)
+    End Sub
+
+    <TestMethod()> Public Sub TestRecibirTodosMensajes()
+        Dim Resultado As Boolean
+        Try
+            c.RecibirTodosMensajes("161348021")
+            Resultado = True
+        Catch ex As Exception
+            Resultado = False
+        End Try
+
+        Assert.IsTrue(Resultado)
+        Thread.Sleep(20)
+    End Sub
+
+    <TestMethod()> Public Sub TestRecibirMensajesPorSesion()
+        Dim Resultado As Boolean
+        Try
+            c.RecibirMensajes("161348021", "M")
             Resultado = True
         Catch ex As Exception
             Resultado = False
